22f295d5e0a6bfc31e125b9df8bb4c0bf3171202
cbullard2 <cbullard2@WLLT-127853>
2023-07-12 16:04:42 -0500

    Add project files.

---
+++ UserCreation and Storage/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace UserCreation_and_Storage
+{
+    using System.Runtime.CompilerServices;
+    using System.Security.Cryptography.X509Certificates;
+    using static System.Console;
+        
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> users = new List<string>();
+
+
+            WriteLine("Would you like to create a new user? Y for yes; N for no.");
+            char yOrN = char.ToLower(char.Parse(ReadLine()));
+
+            if (yOrN == 'y')
+            {
+                char loopResponse = 'y';
+                while (loopResponse == 'y')
+                {
+                    NewUser();
+                    users.Add(userName);
+                }
+            }
+            else
+            {
+                WriteLine("Okay! Goodbye!");
+                
+            }
+
+        }
+
+
+        public static string NewUser()
+        {
+            string userName;
+            WriteLine("Welcome! Please enter your first and last name, separated by a space.");
+            userName = ReadLine();
+            return userName;
+            
+
+
+        }
+    }
+}
+    
